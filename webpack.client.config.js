ddc40b78ab4be64d9c49b073d8234748d7cc31f6
Niklas Persson <equadon@gmail.com>
2017-11-26 13:43:35 +0100

    Client files.

---
+++ webpack.client.config.js
@@ -0,0 +1,39 @@
+const webpack = require('webpack');
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const client = {
+    entry: {
+        app: './client/js/main.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'build/client'),
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/
+        }]
+    },
+    resolve: {
+        modules: [
+            path.resolve(__dirname, 'client/js'),
+            path.resolve(__dirname, 'node_modules')
+        ]
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        }),
+        new HtmlWebpackPlugin({
+            template: './client/index.html'
+        })
+    ]
+};
+
+module.exports = client;
