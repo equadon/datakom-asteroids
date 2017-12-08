7a4f790200eb0cc6d06861c10c67903432f4f3e5
emmatysk <emma@precisit.com>
2017-12-08 22:32:56 +0100

    Added loading icon and timeout functions to delay login

--- client/js/main.js
+++ client/js/main.js
@@ -3,7 +3,7 @@ import Game from 'Game';
 
 
 let game = new Game();
-game.start();
+//game.start();
 
 $(document).ready(function(){
     $("#hide").click(function(){
@@ -25,15 +25,21 @@ $(document).ready(function(){
 
 
 $(document).ready(function() {
+    $("#main").hide();
     $("button").click(function(event){
-        //alert($('#username').val());
+
         var username = $('#username').val();
         var password = $('#password').val();
  
         event.preventDefault();
-        game.client.login(username, password); //Send login to client
-
+        //game.client.login(username, password); //Send login to client
         showGame();
+        setTimeout(function() { $("#loading-icon").hide();  }, 3500);
+        setTimeout(function() { $("#main").show();  }, 4000);
+        setTimeout(function() {   game.start(); }, 4500);
+
+
+        setTimeout(function() {game.client.login(username, password); }, 6000);
     });
 });
 
@@ -41,7 +47,7 @@ function showGame() {
 
     //document.getElementById("gameContainer").style.display = "block";
     window.scroll({
-        top: 2500,
+        top: 2200,
         left: 0,
         behavior: 'smooth'
     });
